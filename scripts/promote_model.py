3c4ea783859d72b5c6812e05a37063ea75b54f21
Tarun Singh <tarunsingh@mac.home>
2026-07-21 04:59:56 +1000

    CI/CD 7

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -13,8 +13,8 @@ def promote_model():
     os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
     dagshub_url = "https://dagshub.com"
-    repo_owner = "vikashdas770"
-    repo_name = "YT-Capstone-Project"
+    repo_owner = "tarun931"
+    repo_name = "MLOps-Capstone-Project"
 
     # Set up MLflow tracking URI
     mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
